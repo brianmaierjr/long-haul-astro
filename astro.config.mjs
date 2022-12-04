407b81ef9a3887cef8c92aeca13ed411742a5ac7
Brian Maier <brimaidesigns@gmail.com>
2022-12-04 00:53:30 -0500

    + compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 
+// https://astro.build/config
 export default defineConfig({
     site: "https://long-haul-astro.netlify.app",
-    integrations: [sitemap()],
+    integrations: [sitemap(), compress()],
     markdown: {
         shikiConfig: {
             // Choose from Shiki's built-in themes (or add your own)
