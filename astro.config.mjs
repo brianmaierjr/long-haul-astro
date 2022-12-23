bf0259b6b13d7264a49ecd85ec8c0d4bc8d80cc2
Brian Maier <brimaidesigns@gmail.com>
2022-12-23 02:34:28 -0500

    so much cleanup, more to come

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,15 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://long-haul-astro.netlify.app",
-  integrations: [sitemap(), robotsTxt(), alpinejs()],
-  markdown: {
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: "nord",
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
+    site: "https://long-haul-astro.netlify.app",
+    integrations: [sitemap(), compress(), robotsTxt(), alpinejs()],
+    markdown: {
+        shikiConfig: {
+            // Choose from Shiki's built-in themes (or add your own)
+            // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+            theme: "nord",
+            // Enable word wrap to prevent horizontal scrolling
+            wrap: true,
+        },
     },
-  },
 });
